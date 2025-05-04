96925eee6659e489efae636cffc13f5593ad6938
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-04 14:53:38 +0500

    Fetching data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
 
 const Home = () => {
@@ -6,13 +6,39 @@ const Home = () => {
   const cardsInrow = 5;
   const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInrow);
 
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const setMovies = async () => {
+
+    const url = 'https://tvshow.p.rapidapi.com/Person/Popular?page=1&language=en-US';
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '7d053995bfmshc22d580fdd0fa8fp150f53jsn1fd9a291d55e',
+        'x-rapidapi-host': 'tvshow.p.rapidapi.com'
+      }
+    };
+
+    try {
+      // const response = await fetch(url, options);
+      const result = await response.json();
+      setMovies(result);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+      setMovies()
+  },[])
+
   return (
-    <div className=" flex flex-center items-center" style={{ width: wrapperWidth }}>
-      <div className="flex flex-wrap">
-        <div>
-          <Cards />
-        </div>
-      </div>
+    <div
+      className=" flex flex-center items-center"
+      style={{ width: wrapperWidth }}>
+      <div className="flex flex-wrap"></div>
     </div>
   );
 };
